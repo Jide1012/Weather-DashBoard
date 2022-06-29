7c64fa0ae0e15ca39cf53841d4b5eeb84aeb68ef
Jide Adesanya <Jide1012@yahoo.com>
2022-06-29 13:59:54 -0500

    fixed css

--- script.js
+++ script.js
@@ -1,4 +1,11 @@
 function initPage() {
+    const currentWindEl = document.getElementById("wind-speed");
+    const currentUVEl = document.getElementById("UV-index");
+    const historyEl = document.getElementById("history");
+    var fivedayEl = document.getElementById("fiveday-header");
+    var todayweatherEl = document.getElementById("todays-weather");
+    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
+
     const cityEl = document.getElementById("enter-city");
     const searchEl = document.getElementById("search-button");
     const clearEl = document.getElementById("clear-history");
@@ -6,12 +13,6 @@ function initPage() {
     const currentPicEl = document.getElementById("current-picture");
     const currentTempEl = document.getElementById("temperature");
     const currentHumidityEl = document.getElementById("humidity");
-    const currentWindEl = document.getElementById("wind-speed");
-    const currentUVEl = document.getElementById("UV-index");
-    const historyEl = document.getElementById("history");
-    var fivedayEl = document.getElementById("fiveday-header");
-    var todayweatherEl = document.getElementById("todays-weather");
-    let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
 }
 // API key by openweathermap 
 const APIKey = "e397c0ad290977cba948a64734ab37cd";
