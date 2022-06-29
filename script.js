2ce90e767bf71d6a9a6d8e6c49f7542d6e589c70
Jide Adesanya <Jide1012@yahoo.com>
2022-06-29 15:35:00 -0500

    fixed html and js

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function initPage() {
-    
     const cityEl = document.getElementById("enter-city");
     const searchEl = document.getElementById("search-button");
     const clearEl = document.getElementById("clear-history");
@@ -13,9 +12,8 @@ function initPage() {
     var fivedayEl = document.getElementById("fiveday-header");
     var todayweatherEl = document.getElementById("todays-weather");
     let searchHistory = JSON.parse(localStorage.getItem("search")) || [];
-
 }
-// Weather API Key
+// Weather API key
 const APIKey = "e397c0ad290977cba948a64734ab37cd";
 const FORECAST_URL = "https://api.openweathermap.org/data/2.5/forecast?appid=" + APIKey + "&units=imperial&q=";
 const CURRENT_URL = "https://api.openweathermap.org/data/2.5/weather?appid=" + APIKey + "&units=imperial&q=";
@@ -25,28 +23,28 @@ const ICON_DOUBLE_SIZE_SUFFIX = "@2x.png";
 
 
 function printFiveDayForecastCard(card, forecast) {
-
+    // 5-day forecast card
     let forecastDate = moment(forecast.dt_txt, "YYYY-MM-DD HH:mm:ss").format("MM/DD/YYYY");
     let temperature = forecast.main.temp;
     let humidity = forecast.main.humidity;
     let weather = forecast.weather[0].description;
     let weatherIcon = ICON_URL + forecast.weather[0].icon + ICON_DOUBLE_SIZE_SUFFIX;
 
-
-    card.innerHTML = "";
-    card.innerHTML += "<p>" + forecastDate + "</p>";
-    card.innerHTML += "<img src=\"" + weatherIcon + "\" alt=\"" + weather + "\"></img>";
-    card.innerHTML += "<p>Temperature: " + temperature + " F</p>";
-    card.innerHTML += "<p>Humidity: " + humidity + " %</p>";
+    // Present data inside HTML
+    card.innerHTML = ""; // Empties existing content from the Card
+    card.innerHTML += "<p>" + forecastDate +"</p>"; // Adds Date
+    card.innerHTML += "<img src=\"" + weatherIcon + "\" alt=\"" + weather + "\"></img>"; // Appends Icon
+    card.innerHTML += "<p>Temperature: " + temperature + " F</p>"; // Appends Temperature in F
+    card.innerHTML += "<p>Humidity: " + humidity + " %</p>"; // And Humidity
 }
 
 function fiveDayForecast(cityName) {
     let forecastQuery = FORECAST_URL + cityName;
-    $.getJSON(forecastQuery, function (data, status) {
-        if (status !== "success") {
-            // show error message
+    $.getJSON(forecastQuery, function(data, status) {
+        if(status !== "works") {
+            //error message
         } else {
-            for (var i = 0; i < 5; i++) {
+            for(var i = 0; i < 5; i++) {
                 printFiveDayForecastCard(document.getElementById("forecast" + i), data.list[i * 8 + 4]); // Multiply by 8 because forecast every 3 hours (8 times a day), and add 4 to point to Noon (as noon is the fourth block of three hours)
             }
         }
@@ -54,9 +52,9 @@ function fiveDayForecast(cityName) {
 }
 
 function currentWeather(cityName) {
-    let weatherQuery = CURRENT_URL + cityName;
-    $.getJSON(weatherQuery, function (data, status) {
-        if (status !== "success") {
+    let weatherQuery = CURRENT_URL + cityName; 
+    $.getJSON(weatherQuery, function(data, status) {
+        if(status !=="works") {
             //error message
         } else {
             let forecast = data;
@@ -78,16 +76,16 @@ function currentWeather(cityName) {
             let lat = forecast.coord.lat;
             let lon = forecast.coord.lon;
             let uviQuery = UVI_URL + "&lat=" + lat + "&lon=" + lon;
-            $.getJSON(uviQuery, function (uviData, uviStatus) {
-                if (uviStatus !== "success") {
-
+            $.getJSON(uviQuery, function(uviData, uviStatus) {
+                if(uviStatus !== "success") {
+                    
                 } else {
                     let uvi = uviData.value;
                     var uvi_rating = "";
-                    if (uvi <= 4.0) {
+                    if(uvi <= 4.0) {
                         uvi_rating = "success";
                     }
-                    else if (uvi <= 8.0) {
+                    else if(uvi <= 8.0) {
                         uvi_rating = "warning";
                     } else {
                         uvi_rating = "danger";
@@ -103,12 +101,12 @@ function currentWeather(cityName) {
 
 function getWeather(cityName) {
     fiveDayForecast(cityName);
-    currentWeather(cityName);
+    currentWeather(cityName);    
 }
 
 function readFromLocalStorage() {
-    var currentState = window.localStorage.getItem("cityHistory");
-    if (!currentState) {
+    var currentState = window.localStorage.getItem("cityHistory"); 
+    if(!currentState) {
         currentState = [];
     } else {
         currentState = JSON.parse(currentState).cities;
@@ -121,24 +119,24 @@ function addCityToLocalStorage(cityName) {
     currentState.unshift(cityName);
     window.localStorage.setItem("cityHistory", JSON.stringify({ cities: currentState }));
 }
-//clear and refresh search history
+//clear and refresh history
 function refreshSearchHistory() {
     let cityHistory = readFromLocalStorage();
     $("#searchHistory")[0].innerHTML = "";
-    for (var i = 0; i < cityHistory.length; i++) {
+    for(var i = 0; i < cityHistory.length; i++) {
         let cityName = cityHistory[i];
         $("#searchHistory")[0].innerHTML += "<div class=\"button\" onClick=\"getWeather('" + cityName + "');\">" + cityName + "</div>";
     }
 }
 
-$("#executeSearch").click(function () {
+$("#executeSearch").click(function() {
     let cityName = $("#cityName")[0].value;
     getWeather(cityName);
     addCityToLocalStorage(cityName);
     refreshSearchHistory();
 });
 
-$("#clear-history-btn").click(function () {
+$("#clear-history-btn").click(function() {
     window.localStorage.setItem("cityHistory", JSON.stringify({ cities: [] }));
     refreshSearchHistory();
 });
